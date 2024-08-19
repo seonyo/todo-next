11137f431b3773461e87368ce9c94cc4d9a88f80
seonyo <jsy6535@naver.com>
2024-08-20 03:51:32 +0900

    home 퍼블리싱 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Title from "@/components/title";
 
 export default function Home() {
-    return(
-        <Title title="What are you going to do?" />
+    return (
+        <div className="flex justify-center">
+            <Title title="What are you going to do?" />
+            <input type="text"
+                className="w-2/5 h-16 rounded-full border border-mainBlue fixed mt-80 z-10 pl-6 focus:outline-none text-neutral-700">
+            </input>
+            <button type="submit"
+                className="w-32 h-10 rounded-full font-light bg-mainBlue absolute text-white mt-[420px]"
+            >Submit</button>
+        </div>
     )
 }
